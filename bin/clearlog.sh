dfd4ccc3d8fdb5d781d3b670cb47c79edbb87777
Will Shi <drinkthere@gmail.com>
2025-04-27 09:17:53 +0800

    add sh

--- bin/clearlog.sh
+++ bin/clearlog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # 指定日志文件所在的目录
-spot_log_directory="/data/will/syncPrice"
-+ls -t $spot_log_directory/bbmms-bb034.log* | tail -n +5 | xargs rm -f
+spot_log_directory="/data/dc/syncPrice"
++ls -t $spot_log_directory/syncprice.log* | tail -n +5 | xargs rm -f
---
+++ bin/manage.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+$cpuidx=0
+
+start() {
+    echo "Starting the process..."
+    nohup  taskset -c "$cpuidx" ./syncprice ../config/config.json >> /data/dc/syncPrice/nohup.log 2>&1 &
+    echo "Process started."
+}
+
+stop() {
+    echo "Stopping the process..."
+    pid=$(pgrep -f "syncprice ../config/config.json")
+    if [ -n "$pid" ]; then
+        kill -SIGINT $pid
+        echo "Process stopping: "
+        sleep 1
+        echo "Process stopped."
+    else
+        echo "Process is not running."
+    fi
+}
+
+restart() {
+    stop
+    sleep 10
+    start
+}
+
+case "$2" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        restart
+        ;;
+    *)
+        echo "Usage: $0 account {start|stop|restart}"
+        exit 1
+        ;;
+esac
+
+exit 0
